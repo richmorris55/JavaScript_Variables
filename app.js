62459908a3cb686d75fbfbb6ccec45b2353fba48
Richard Morris <richmorris55@yahoo.com>
2023-03-09 08:31:02 -0800

    InitialCommit

--- app.js
+++ app.js
@@ -3,8 +3,52 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 and 2
 
-// Exercise 2
+const firstName = "Richard";
+
+let lastName = "Morris";
+
+var age = 33;
+
+console.log(`Hello my name is ${firstName} ${lastName}. I am ${age} years old.`);
 
 // Exercise 3
+
+let language = "JavaScript";
+
+let CreatedYear = 1995;
+
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${CreatedYear} and it is ${isCaseSensitive} that it is a case sensitive language.`);
+
+// Excercise 4
+
+let price = 19.99;
+
+let isOnSale = false;
+
+let salePercentage = 15;
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+console.log(`$Price: ${price}, On sale:${isOnSale}, Savings:${salePercentage} percent, Stock:${stock}, In stock:${inStock} Size:${selectedSize}`);
+
+//Exercise 5
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
+
+console.log(`$It is ${hasRead} there is a good read called ${title} written by ${author}. It has ${pageCount} pages. I am on page ${bookmark}. I am exited to read what is about to happen next!`);
